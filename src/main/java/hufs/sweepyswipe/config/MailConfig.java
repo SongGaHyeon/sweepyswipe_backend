1035670693692093647dc77c0434487a16e1d4d9
hajin <kimhajin01@naver.com>
2024-06-09 03:46:11 +0900

    gitignore test

---
+++ src/main/java/hufs/sweepyswipe/config/MailConfig.java
@@ -0,0 +1,9 @@
+package hufs.sweepyswipe.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@Configuration
+public class MailConfig {
+
+}
