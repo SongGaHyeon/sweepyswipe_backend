96e1723352f0e1bef9f75f1613475b818767d839
hajin <kimhajin01@naver.com>
2024-06-10 00:33:15 +0900

    delete webconfig path

--- src/main/java/hufs/sweepyswipe/config/WebConfig.java
+++ src/main/java/hufs/sweepyswipe/config/WebConfig.java
@@ -10,6 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginCheckInterceptor())
-                .addPathPatterns("/mypage/**", "/members/alarm/**");
+                .addPathPatterns("/members/alarm/**");
     }
 }
